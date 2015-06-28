6f655f365366a48c39f27f0527d9c8bba67a0cea
Dablu Kumar <dabluk@thoughtworks.com>
2015-06-28 20:37:10 +0530

    Agency main file modified for getting district after state population

--- src/main/AgencyApp.java
+++ src/main/AgencyApp.java
@@ -10,21 +10,54 @@ import java.util.HashMap;
  */
 public class AgencyApp {
     private static HashMap<String,Integer> statePopulationData;
+    private static HashMap<String,Integer> districtPopulationData;
     public static void main(String args[]){
         createStatePopulationData();
+        createDistrictPopulationData();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StatePopulation statePopulation = new StatePopulation(statePopulationData);
         while(true) {
-
             try {
                 System.out.println("Enter the State Name for getting Population");
                 String stateName = br.readLine();
                 int population = statePopulation.getPopulation(stateName);
                 System.out.println(population);
+                System.out.println("Want To check " + stateName + "District Population Press Y");
+                String check = br.readLine();
+                if(check.equals("Y")){
+                    getDistrictPopulation();
+                }
+                System.out.println("Press N to exit the App or Y to continue");
+                String status = br.readLine();
+                if (status.equals("N"))
+                    System.exit(0);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void getDistrictPopulation() {
+        DistrictPopulation districtPopulation = new DistrictPopulation(districtPopulationData);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.println("Enter the District Name for getting Population");
+            String stateName = br.readLine();
+            int population = districtPopulation.getPopulation(stateName);
+            System.out.println(population);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+    public  static void createDistrictPopulationData(){
+        districtPopulationData = new HashMap<>();
+        districtPopulationData.put("pune",1000);
+        districtPopulationData.put("patna",1000);
+        districtPopulationData.put("bangalore",1000);
+        districtPopulationData.put("chennai",1000);
+        districtPopulationData.put("bhubneshwar",1000);
     }
 
     private static void createStatePopulationData() {
@@ -33,6 +66,7 @@ public class AgencyApp {
         statePopulationData.put("maharashtra",1500);
         statePopulationData.put("bihar",1200);
         statePopulationData.put("tamilnadu",1300);
-        statePopulationData.put("gujrat",2000);
+        statePopulationData.put("gujrat", 2000);
     }
+
 }
