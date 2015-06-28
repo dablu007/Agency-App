2f4340e340cacce941f08bb66bda1f9f7cd52bac
Dablu Kumar <dabluk@thoughtworks.com>
2015-06-28 20:59:34 +0530

    Districts for state file added and tested for diffrent test cases

---
+++ src/main/StateDistrict.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by dabluk on 28/06/15.
+ */
+public class StateDistrict {
+    HashMap<String,ArrayList<String>> stateDistrictData;
+    public StateDistrict(HashMap<String, ArrayList<String>> data) {
+        stateDistrictData = new HashMap<>();
+        stateDistrictData.putAll(data);
+    }
+
+    public ArrayList<String> getDistrictForState(String name) {
+        if (stateDistrictData.containsKey(name))
+            return stateDistrictData.get(name);
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/TestStateDistrict.java
@@ -0,0 +1,52 @@
+package test;
+
+import main.StateDistrict;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by dabluk on 28/06/15.
+ */
+public class TestStateDistrict {
+    StateDistrict stateDistrict;
+    HashMap<String,ArrayList<String>> stateDistrictData;
+    ArrayList<String> districts;
+    ArrayList<String> anotherDistricts;
+    @Before
+    public void setup(){
+        districts = new ArrayList<>();
+        anotherDistricts = new ArrayList<>();
+        stateDistrictData = new HashMap<>();
+        districts.add("thane");
+        districts.add("pune");
+        anotherDistricts.add("patna");
+        anotherDistricts.add("samastipur");
+    }
+    @Test
+    public void testShouldReturnDistrict(){
+        stateDistrictData.put("maharashtra", new ArrayList<String>());
+        stateDistrict = new StateDistrict(stateDistrictData);
+        assertEquals(null,stateDistrict.getDistrictForState("maharashtra"));
+
+    }
+
+    @Test
+    public void testShouldReturnDistrictForState() {
+        stateDistrictData.put("maharashtra",districts);
+        stateDistrictData.put("bihar",anotherDistricts);
+        stateDistrict = new StateDistrict(stateDistrictData);
+        assertEquals(anotherDistricts, stateDistrict.getDistrictForState("bihar"));
+    }
+
+    @Test
+    public void testShouldNotReturnNullWhenStateNotPresent() {
+        stateDistrictData.put("maharashtra",districts);
+        stateDistrict = new StateDistrict(stateDistrictData);
+        assertEquals(new ArrayList<String>(), stateDistrict.getDistrictForState("karnataka"));
+    }
+}
